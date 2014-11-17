b333d2c83688e2b2b2a7199b67a1d9eaca0ef172
Lee Whittaker <lee@21zero.com>
2014-11-16 19:30:49 -0700

    Unity XML Parsing script, fully functioning.

---
+++ UnityProject-ZombieKitty/Assets/Scripts/PuzzleParser.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class PuzzleParser : MonoBehaviour {
+	
+	public Library allPuzzles;
+	private string Data, _Location;
+	[SerializeField]
+	private string Filename;
+
+	void Start()
+	{
+		_Location = Application.dataPath + "\\XML";
+		LoadPuzzles();
+		if(Data.ToString() != "")
+		{
+			allPuzzles = (Library)DeserializeObject(Data);
+
+			foreach(Level level in allPuzzles.levels)
+			{
+				foreach(Puzzle puzzle in level.puzzles)
+				{
+					foreach (Row row in puzzle.rows)
+					{
+						foreach (Cell cell in row.cells)
+						{
+							Debug.Log (cell.getColor());
+						}
+					}
+				}
+			}
+		}
+	}
+
+	byte[] StringToUTF8ByteArray(string pXmlString)
+	{
+		UTF8Encoding encoding = new UTF8Encoding();
+		byte[] byteArray = encoding.GetBytes(pXmlString);
+		return byteArray;
+	}
+
+	object DeserializeObject (string pXmlizedString)
+	{
+		XmlSerializer xs = new XmlSerializer(typeof(Library));
+		MemoryStream memoryStream = new MemoryStream(StringToUTF8ByteArray(pXmlizedString));
+		XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
+		return xs.Deserialize(memoryStream);
+	}
+
+	void LoadPuzzles()
+	{
+		StreamReader r = File.OpenText(_Location + "\\" + Filename);
+		string info = r.ReadToEnd();
+		r.Close();
+		Data = info;
+	}
+}
+
+[XmlRoot("root")]
+public class Library
+{
+	[XmlElement("level")]
+	public List<Level> levels { get; set; }
+
+	public Library()
+	{
+		levels = new List<Level>();
+	}
+}
+
+public class Level
+{
+	[XmlAttribute("levelnum")]
+	public int levelnum { get; set; }
+
+	[XmlElement("puzzle")]
+	public List<Puzzle> puzzles { get; set; }
+
+	public bool unlocked { get; set; }
+
+	public Level()
+	{
+		unlocked = false;
+		puzzles = new List<Puzzle>();
+	}
+}
+
+public class Puzzle
+{
+	[XmlAttribute("name")]
+	public string name { get; set; }
+
+	[XmlAttribute("puzzlenum")]
+	public int puzzlenum { get; set; }
+
+	[XmlElement("row")]
+	public List<Row> rows { get; set; }
+
+	public bool unlocked { get; set; }
+
+	public Clues rowClues, colClues;
+
+	public Puzzle()
+	{
+		unlocked = false;
+		rows = new List<Row>();
+	}
+
+	public Clues getClues(string type)
+	{
+		Clues c;
+		switch(type)
+		{
+		case "row":
+			//Generate Row Clues
+			break;
+		case "column":
+			//Generate Column Clues
+			break;
+		default:
+			break;
+		}
+		return c;
+	}
+}
+
+public class Row
+{
+	[XmlElement("cell")]
+	public List<Cell> cells { get; set; }
+
+	public Row()
+	{
+		cells = new List<Cell>();
+	}
+}
+
+public class Cell
+{
+	[XmlAttribute("color")]
+	public string color { get; set; }
+	
+	[XmlText]
+	public string health { get; set; }
+
+	public Cell()
+	{
+		color = "";
+	}
+
+	public bool isHealthy()
+	{
+		return (health == "true") ? true : false;
+	}
+
+	public Color getColor()
+	{
+		if(color.Contains ("rgb"))
+		{
+			MatchCollection colors = Regex.Matches (color, "[0-9]+");
+			float r = float.Parse(colors[0].Value);
+			float g = float.Parse(colors[1].Value);
+			float b = float.Parse(colors[2].Value);
+			Color c = new Color(r/255.0F,g/255.0F,b/255.0F,1);
+			return c;
+		}
+		else
+		{
+			return Color.clear;
+		}
+	}
+}
+
+public class Clues
+{
+	public List<List<int>> clueList;
+
+	public Clues()
+	{
+		clueList = new List<List<int>>();
+	}
+}
