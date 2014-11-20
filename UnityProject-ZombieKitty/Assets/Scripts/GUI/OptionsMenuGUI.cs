446b26769ab744cc05cd93478ff3dc44874b3359
Lee Whittaker <lee@21zero.com>
2014-11-20 16:45:34 -0700

    Cleaned up code and added numbers to the sliders in the Options menu

--- UnityProject-ZombieKitty/Assets/Scripts/GUI/OptionsMenuGUI.cs
+++ UnityProject-ZombieKitty/Assets/Scripts/GUI/OptionsMenuGUI.cs
@@ -8,7 +8,7 @@ public class OptionsMenuGUI : MonoBehaviour {
 	private GameSettings settings;
 	private string[] modes = new string[] {"Highlight", "Direct"};
 
-	void Start() {
+	void OnEnable() {
 		settings = GameObject.Find ("PlayerData").
 			GetComponent<PlayerData>().
 				CurrentPlayer.
@@ -16,21 +16,16 @@ public class OptionsMenuGUI : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		settings = GameObject.Find ("PlayerData").
-			GetComponent<PlayerData>().
-				CurrentPlayer.
-				settings;
-
 		if(GUI.Button (MainMenu.rect, MainMenu.title))
 		{
 			GameObject.Find ("PlayerData").GetComponent<PlayerData>().SaveData();
 			MenuController.ChangeMenu(MainMenu.menuObject, this.gameObject);
 		}
 
-		settings.musicVolume = GUI.HorizontalSlider(new Rect (25,25,100,30), settings.musicVolume, 0.0f, 1.0f);
-		GUI.Label(new Rect(25, 50, 100, 30), "Music Volume");
-		settings.sfxVolume = GUI.HorizontalSlider(new Rect (25,75,100,30), settings.sfxVolume, 0.0f, 1.0f);
-		GUI.Label(new Rect(25, 100, 100, 30), "SFX Volume");
+		settings.musicVolume = GUI.HorizontalSlider(new Rect (25,25,150,30), settings.musicVolume, 0.0f, 1.0f);
+		GUI.Label(new Rect(25, 50, 150, 30), "Music Volume: " + Mathf.RoundToInt(settings.musicVolume * 100));
+		settings.sfxVolume = GUI.HorizontalSlider(new Rect (25,75,150,30), settings.sfxVolume, 0.0f, 1.0f);
+		GUI.Label(new Rect(25, 100, 150, 30), "SFX Volume: " + Mathf.RoundToInt(settings.sfxVolume * 100));
 		settings.playmode = GUILayout.SelectionGrid(settings.playmode, modes, modes.Length);
 	}
 }
