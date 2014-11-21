2086e52a882bc96d569aa43f8705c01c840057ef
Lee Whittaker <lee@21zero.com>
2014-11-21 07:53:51 -0700

    Added puzzle cell prefab.

---
+++ UnityProject-ZombieKitty/Assets/BoxBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxBehaviour : MonoBehaviour {
+	
+	public delegate void myDelegate();
+	myDelegate clickEvent, watch;
+
+	public Material openBox;
+	public Color openColor;
+	public bool kitty;
+	public string state = "hand";
+	public int mode = 1;
+
+	void Start()
+	{
+		switch (mode)
+		{
+		case 1:
+			clickEvent = direct;
+			break;
+		case 0:
+			clickEvent = highlight;
+			break;
+		default:
+			break;
+		}
+		watch = empty;
+	}
+
+	void Update()
+	{
+		watch();
+	}
+
+	void OnMouseOver()
+	{
+		if(Input.GetMouseButton(0))
+			clickEvent();
+	}
+
+	void direct()
+	{
+		switch (state)
+		{
+		case "hand":
+			// Hand events
+			break;
+		case "shotgun":
+			// Shotgun events
+			break;
+		default:
+			break;
+		}
+
+		this.renderer.material = openBox;
+		this.renderer.material.color = openColor;
+		clickEvent = empty;
+	}
+	void highlight()
+	{
+	
+	}
+
+	void empty (){}
+}
